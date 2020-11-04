0d28da5751ef13d7ea03d679f610c3ea7201f355
Renato Welinton Schlogel <renato.s@outlook.com>
2020-11-03 23:52:49 -0300

    Seção 5: Spy

--- TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
+++ TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
@@ -1,17 +1,46 @@
 package br.ce.wcaquino.servicos;
 
+import static org.junit.Assert.*;
+
 import java.math.BigDecimal;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-
-import br.ce.wcaquino.entidades.Locacao;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 
 public class CalculadoraMockTest {
 
+	@Mock
+	private Calculadora calcMock;
+
+	@Spy
+	private Calculadora calcSpy;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		
+	}
+	
+	@Test
+	public void devoMostrarADifrerencaEntreMockESpy() throws Exception {
+		
+		Mockito.when(calcMock.somar(new BigDecimal(1), new BigDecimal(2))).thenReturn(new BigDecimal(3));
+		Mockito.when(calcSpy.somar(new BigDecimal(1), new BigDecimal(2))).thenReturn(new BigDecimal(3));
+		
+		Mockito.doReturn(new BigDecimal(5)).when(calcSpy).somar(new BigDecimal(2), new BigDecimal(3));
+		
+		
+		System.out.println("Mock: " +  calcMock.somar(new BigDecimal(1), new BigDecimal(2)));
+		System.out.println("Spy: " +  calcSpy.somar(new BigDecimal(1), new BigDecimal(2)));
+	}
+	
 	@Test
 	public void testName() throws Exception {
 		
